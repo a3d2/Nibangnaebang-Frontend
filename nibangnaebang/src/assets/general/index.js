9891fb594b78b1d1f00d63690b31e1e476b08152
Peter <uniqueimaginate@gmail.com>
2019-06-28 03:21:54 +0900

    detail

--- nibangnaebang/src/assets/general/index.js
+++ nibangnaebang/src/assets/general/index.js
@@ -5,6 +5,7 @@ const assets = {
     iconFilter: require('./iconFilter.png'),
     iconMap: require('./iconMap.png'),
     iconCalculator: require('./iconCalculator.png'),
+    iconFace: require('./iconFace.png'),
 };
 
 export default assets
