52d40ae8d0317194348c127edc6a246b335135fe
yjb94 <jbyun94@naver.com>
2019-06-27 17:58:43 +0900

    upload step detail

--- nibangnaebang/src/screens/Tabs/UploadTab/Upload/UploadStepDesc.js
+++ nibangnaebang/src/screens/Tabs/UploadTab/Upload/UploadStepDesc.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { inject } from 'mobx-react';
 import PropTypes from 'prop-types'
 import styled from 'styled-components/native';
+import ImagePicker from 'react-native-image-crop-picker';
+import TextInput from '../../../../components/data/input/TextInput';
+import CheckButton from '../../../../components/feedback/button/CheckButton';
 
 @inject(stores => ({
     navTo:stores.nav.navTo,
@@ -11,12 +14,69 @@ class UploadStepDesc extends React.Component {
         super(props);
 
         this.state = {
+            images:[],
+            cautionChecked:false
         }
     }
+
+    openPicker = () => {
+        ImagePicker.openPicker({
+            multiple:true
+        }).then(images => {
+            this.setState({ images:images })
+        });
+    }
     
+    toggleCheckBox = () => {
+        const { cautionChecked } = this.state;
+        this.setState({ cautionChecked:!cautionChecked });
+    }
+
     render() {
+        const { images, cautionChecked } = this.state;
+
+        const roomImages = images.map((each, idx) => {
+            console.log("TCL: UploadStepDesc -> render -> each", each)
+            return (
+                <RoomImageContainer key={idx}>
+                    <RoomImage source={{
+                        uri:each.path
+                    }}/>
+                </RoomImageContainer>
+            )
+        })
+
         return (
             <Container>
+                <RoomImagesContainer
+                    horizontal
+                >
+                    <AddRoomImageButton
+                        onPress={this.openPicker}
+                    >
+                        <AddRoomImageButtonText>
+                            추가
+                        </AddRoomImageButtonText>
+                    </AddRoomImageButton>
+                    {roomImages}
+                </RoomImagesContainer>
+                <InputsContainer>
+                    <TextInput
+                        placeholder={'주소'}
+                    />
+                    <TextInput
+                        placeholder={'제목'}
+                    />
+                    <TextInput
+                        placeholder={'방을 자유롭게 소개해주세요'}
+                        multiline
+                    />
+                </InputsContainer>
+                <CheckButton
+                    onPress={this.toggleCheckBox}
+                    checked={cautionChecked}
+                    label={'몰라 일단 체크'}
+                />
             </Container>
         );
     }
@@ -29,6 +89,23 @@ UploadStepDesc.defaultProps = {
 
 const Container = styled.ScrollView`
     flex:1;
+`;
+
+const RoomImagesContainer = styled.ScrollView`
+    flex-direction:row;
+`
+const AddRoomImageButton = styled.TouchableOpacity`
+`;
+const AddRoomImageButtonText = styled.Text`
+`;
+const RoomImageContainer = styled.View`
+`;
+const RoomImage = styled.Image`
+    width:50;
+    height:50;
+`;
+
+const InputsContainer = styled.View`
 `;
 
 export default UploadStepDesc;
